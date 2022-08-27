e52d376fcd536842d725dd42cb69a58383242246
LAPTOP-HKB7FIRP\최준우 <jun_chois@naver.com>
2022-08-27 23:52:46 +0900

    [gitpher] <feat> ChatController

---
+++ src/main/java/com/gitpher/websocket4test/ChatController.java
@@ -0,0 +1,24 @@
+package com.gitpher.websocket4test;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestBody String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRooms() {
+        return chatService.findAllRoom();
+    }
+}
