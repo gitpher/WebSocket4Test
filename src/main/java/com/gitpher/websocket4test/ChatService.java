78ba17f9178b3769e609d05fb91dd86d03ad0ada
LAPTOP-HKB7FIRP\최준우 <jun_chois@naver.com>
2022-08-27 21:18:10 +0900

    [gitpher] <feat> ChatMessage

---
+++ src/main/java/com/gitpher/websocket4test/ChatService.java
@@ -0,0 +1,9 @@
+package com.gitpher.websocket4test;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ChatService {
+}
