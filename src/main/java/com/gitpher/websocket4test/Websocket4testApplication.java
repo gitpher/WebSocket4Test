c31b2292807c7c50586a54c396cfb2ab485cd4b7
LAPTOP-HKB7FIRP\최준우 <jun_chois@naver.com>
2022-08-27 20:21:10 +0900

    [gitpher] <create> project frame for websocket test

---
+++ src/main/java/com/gitpher/websocket4test/Websocket4testApplication.java
@@ -0,0 +1,13 @@
+package com.gitpher.websocket4test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Websocket4testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Websocket4testApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitpher/websocket4test/Websocket4testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitpher.websocket4test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Websocket4testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
